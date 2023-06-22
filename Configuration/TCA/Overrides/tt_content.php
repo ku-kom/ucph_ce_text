87f8f622ef576eaa5c43adbec21821937c11f101
nanna-dk <nanna@e-nanna.dk>
2023-06-22 13:22:54 +0200

    Init

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -6,9 +6,57 @@
  * LICENSE file that was distributed with this source code.
  * June 2023 Nanna Ellegaard, University of Copenhagen.
  */
-
+declare(strict_types=1);
 defined('TYPO3') or die();
 
-(function () {
+call_user_func(function ($extKey ='ucph_ce_text', $contentType ='ucph_ce_regular_text') {
+    // Adds the content element to the "Type" dropdown
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+        'tt_content',
+        'CType',
+        [
+            'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:ucph_ce_text_title',
+            $contentType,
+            // icon identifier
+            'content-text',
+        ],
+        'textmedia',
+        'after'
+    );
 
-})();
+    // Add Content Element
+    if (!is_array($GLOBALS['TCA']['tt_content']['types'][$contentType] ?? false)) {
+        $GLOBALS['TCA']['tt_content']['types'][$contentType] = [];
+    }
+
+    // Configure the default backend fields for the content element
+    $GLOBALS['TCA']['tt_content']['types'][$contentType] = [
+        'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+               --palette--;;general,
+               header; LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:ucph_ce_text_title,
+               bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+               --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+               --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+               --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+               --palette--;;language,
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+               --palette--;;hidden,
+               --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+               categories,
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+               rowDescription,
+           --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+         ',
+        'columnsOverrides' => [
+            'bodytext' => [
+                'config' => [
+                    'enableRichtext' => true,
+                    'richtextConfiguration' => 'default',
+                ],
+            ],
+        ],
+    ];
+});
