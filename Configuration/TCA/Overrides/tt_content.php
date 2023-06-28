89e9e74c276645170fa5ffec4e0782fc84d075be
nanna-dk <nanna@e-nanna.dk>
2023-06-28 11:16:52 +0200

    Setup

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -9,7 +9,7 @@
 declare(strict_types=1);
 defined('TYPO3') or die();
 
-call_user_func(function ($extKey ='ucph_ce_text', $contentType ='ucph_ce_regular_text') {
+call_user_func(function ($extKey ='ucph_ce_text', $contentType ='ucph_ce_text') {
     // Adds the content element to the "Type" dropdown
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
         'tt_content',
